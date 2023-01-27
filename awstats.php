17cbc9226e149a4761406b5cf3311381757d09c6
Telartis <jeroen@telartis.nl>
2023-01-27 19:05:01 +0100

    Update awstats.php

--- awstats.php
+++ awstats.php
@@ -6,30 +6,15 @@
  * In your AWStats.conf set the following:
  * WrapperScript="awstats.php"
  *
- * @author      Jeroen de Jong <jeroen@telartis.nl>
- * @copyright   2004-2022 Telartis BV
- * @version     1.2
- *
- * @link        https://www.telartis.nl/en/awstats
+ * @author     Jeroen de Jong <jeroen@telartis.nl>
+ * @copyright  2004-2023 Telartis BV
+ * @version    1.2
+ * @link       https://www.telartis.nl/en/awstats
  *
  * Changelog:
  * 1.0 initial version
  * 1.1 changed month param pattern
  * 1.2 added type declarations to function arguments and return values
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
 
@@ -66,6 +51,7 @@ function addparam(string $name, string $pattern, bool $allways = false): string
             $result .= ($allways ? '' : ' -'.$name).'='.$_GET[$name];
         }
     }
+
     return $result;
 }
 
