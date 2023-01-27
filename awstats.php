88e174b988c38c87210b8eae064f91bbeb246934
Telartis <jeroen@telartis.nl>
2023-01-27 22:44:55 +0100

    Update awstats.php

--- awstats.php
+++ awstats.php
@@ -1,61 +1,20 @@
-<?php
+<?php declare(strict_types=1);
 
 /**
- * AWStats PHP Wrapper Script
- *
- * In your AWStats.conf set the following:
- * WrapperScript="awstats.php"
- *
+ * Project:    AWStats PHP Wrapper Script
+ * File:       awstats.php
  * @author     Jeroen de Jong <jeroen@telartis.nl>
  * @copyright  2004-2023 Telartis BV
- * @version    1.2
- * @link       https://www.telartis.nl/en/awstats
- *
- * Changelog:
- * 1.0 initial version
- * 1.1 changed month param pattern
- * 1.2 added type declarations to function arguments and return values
+ * @version    1.0
  *
  */
 
-/**
- * The location of the AWStats script.
- */
-$AWStatsFile = '/usr/local/awstats/cgi-bin/awstats.pl';
+require_once 'awstats.class.php';
 
-
-$param = addparam('config', '/^[-\.a-z0-9]+$/i');
-if (empty($param)) {
-    die("config parameter not set!");
-}
-$param .= addparam('output', '/^[a-z0-9]+$/', true);
-$param .= addparam('year',   '/^\d{4}$/');
-$param .= addparam('month',  '/^(\d{1,2}|all)$/');
-$param .= addparam('lang',   '/^[a-z]{2}$/');
-$param .= addfilterparam('hostfilter');
-$param .= addfilterparam('hostfilterex');
-$param .= addfilterparam('urlfilter');
-$param .= addfilterparam('urlfilterex');
-$param .= addfilterparam('refererpagesfilter');
-$param .= addfilterparam('refererpagesfilterex');
-$param .= addfilterparam('filterrawlog');
-
-passthru('perl '.$AWStatsFile.$param);
-
-
-function addparam(string $name, string $pattern, bool $allways = false): string
+class my_awstats extends awstats
 {
-    $result = $allways ? ' -'.$name : '';
-    if (isset($_GET[$name])) {
-        if (preg_match($pattern, $_GET[$name])) {
-            $result .= ($allways ? '' : ' -'.$name).'='.$_GET[$name];
-        }
-    }
-
-    return $result;
+    // public $AWStatsFile = '/usr/local/awstats/cgi-bin/awstats.pl';
 }
 
-function addfilterparam(string $name): string
-{
-    return addparam($name, '/^[^;:,`| ]+$/');
-}
+$awstats = new my_awstats();
+$awstats->main();
